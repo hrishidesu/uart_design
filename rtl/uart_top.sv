853b434c28057521da607f8822a7e47f4c9e6d8a
HrishiKesh Kumar Baruah <kumarbaruah.hrishikesh@gmail.com>
2025-12-01 03:54:47 +0530

    Unverified uart_top.sv added

--- rtl/uart_top.sv
+++ rtl/uart_top.sv
@@ -15,4 +15,34 @@ module uart_top (
     output logic rx_done
 );
 
+    logic s_tick;
+
+    baud_gen #(
+        .WIDTH(14)
+    ) baud_gen_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .dvsr(dvsr),
+        .tick(s_tick)
+    );
+
+    uart_tx uart_tx_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .s_tick(s_tick),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tx(tx),
+        .tx_done(tx_done)
+    );
+
+    uart_rx uart_rx_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .s_tick(s_tick),
+        .rx(rx),
+        .rx_data(rx_data),
+        .rx_done(rx_done)
+    );
+
 endmodule
